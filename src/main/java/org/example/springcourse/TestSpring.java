a9d8b5c669044cbedb30402e6dc0cc2ae5a1310f
Serhii_Savchenko <sgsavch75@gmail.com>
2023-08-03 18:02:25 +0300

    First commit

--- src/main/java/org/example/springcourse/TestSpring.java
+++ src/main/java/org/example/springcourse/TestSpring.java
@@ -10,7 +10,8 @@ public class TestSpring {
 
         Music music = context.getBean("musicBean", Music.class);
 
-        System.out.println(music.getSong());
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
 
         context.close();
     }
