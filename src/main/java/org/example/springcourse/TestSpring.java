511e393c767e65eff96bf99f5b244284fea37d78
Serhii_Savchenko <sgsavch75@gmail.com>
2023-08-09 17:07:51 +0300

    SCOPE Singleton

--- src/main/java/org/example/springcourse/TestSpring.java
+++ src/main/java/org/example/springcourse/TestSpring.java
@@ -9,11 +9,23 @@ public class TestSpring {
         );
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
 
+//        boolean comparison = musicPlayer == musicPlayer1;
+
+        musicPlayer.setVolume(20);
         musicPlayer.playMusicList();
         System.out.println("~~~~~~");
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
+        System.out.println("~~~~~~");
+        System.out.println(musicPlayer == musicPlayer1);
+        System.out.println(musicPlayer);
+        System.out.println(musicPlayer1);
+        System.out.println("~~~~~~");
+        System.out.println(musicPlayer.getVolume());
+        System.out.println(musicPlayer1.getVolume());
+
 
         context.close();
     }
